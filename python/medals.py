86d0720a85fbe73f6041979136320ba42763bb5e
mjafar1000 <mjafar@mail.com>
2021-09-10 15:26:42 +0100

    updated code in file

--- python/medals.py
+++ python/medals.py
@@ -17,10 +17,70 @@ medalResults = [
     }
 ]
 
-def createMedalTable(results):
-    # Use the results object above to create a medal table
-    # The winner gets 3 points, second place 2 points and third place 1 point
-    return
+
+medalResults = [
+    {
+        "sport": "cycling",
+        "podium": ["1.China", "2.Germany", "3.ROC"]
+    },
+    {
+        "sport": "fencing",
+        "podium": ["1.ROC", "2.France", "3.Italy"]
+    },
+    {
+        "sport": "high jump",
+        "podium": ["1.Italy", "1.Qatar", "3.Belarus"]
+    },
+    {
+        "sport": "swimming",
+        "podium": ["1.USA", "2.France", "3.Brazil"]
+    }
+]
+
+
+
+firstlist = []
+secondlist = []
+thirdlist = []
+
+for x in medalResults[0:]:
+    
+    for p in x["podium"]:
+        if "1" in p:
+            firstlist.append(p)
+
+        elif "2" in p:
+            secondlist.append(p)
+        else:
+            thirdlist.append(p)          
+
+table = (firstlist,secondlist,thirdlist)
+
+nt = {}
+
+for country in table[0]:
+    if country[2:] in nt:
+        nt[(country[2:])] += 3
+    else:
+        nt[(country[2:])] = 3
+        
+for country in table[1]:
+    if country[2:] in nt:
+        nt[(country[2:])] += 2
+    else:
+        nt[(country[2:])] = 2
+
+for country in table[2]:
+    if country[2:] in nt:
+        nt[(country[2:])] += 1
+    else:
+        nt[(country[2:])] = 1
+
+
+data_sorted = {k: v for k, v in sorted(nt.items(), key=lambda x: x[1], reverse = True)}
+print("{" + "\n".join("{!r}: {!r},".format(k, v) for k, v in data_sorted.items()) + "}")
+
+
 
 
 def test_function():
